5fe9ef9b2ff40fc828bc5c10f9c3441d319fcb12
Jose Carlos de Moura e Silva <jose.silva@cedrotech.com>
2019-04-02 17:00:26 -0300

    Create tables to working with data base.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@127.0.0.1:53646/Py_Flask_DB'
  
 from app.controllers import default
