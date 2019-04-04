127d946df94da0ce59daec84a5bf5e52dc0f0b13
Jose Carlos de Moura e Silva <jose.silva@cedrotech.com>
2019-04-04 17:05:42 -0300

    Included in project the libraries of de CSS and Java Script from the Bootstrap. Was also created single page to show some modifications that the CSS already brought.

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,10 +1,28 @@
+from flask import render_template
 from app import app
 
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "Hello Word!"
+@app.route("/index/<user>")
+@app.route("/", defaults = {"user" : None})
+def index(user):
+    #Function to retur and render tamplate or simple page HTML
+    #Send the parameter user to template to do binding
+    return render_template('index.html', user = user)
+
+#Sample with how return a single page HTML
+# @app.route("/index")
+# @app.route("/")
+# def index():
+#     return """
+#     <html>
+#         <head>
+#             <title>Hello World!</title>
+#         </head>
+#         <body>
+#             <h1>Hello world!</h1>
+#         </body>
+#     </html>
+#     """
 
 
 #End point send a string with parameter
@@ -15,6 +33,7 @@ def test(name):
         return "Olá, %s!" % name
     else:
         return "Olá usuário!"
+
 
 #End point send a int number with parameter
 @app.route("/number_int/<int:number>")
