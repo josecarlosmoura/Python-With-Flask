b923efb2222e15d4dcd8f2b752bc2b99fad140f9
Jose Carlos de Moura e Silva <jose.silva@cedrotech.com>
2019-04-04 16:11:45 -0300

    Content about routes and a overwiew about methods (POST, PUT, GET and DELETE).

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,22 @@
 from app import app
 
 
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hello Word!"
+
+
+#End point send a string with parameter
+@app.route("/user", defaults = {'name' : None})
+@app.route("/user/<name>")
+def test(name):
+    if name:
+        return "Olá, %s!" % name
+    else:
+        return "Olá usuário!"
+
+#End point send a int number with parameter
+@app.route("/number_int/<int:number>")
+def number_int(number):
+        return "Você digitou %i." % number
